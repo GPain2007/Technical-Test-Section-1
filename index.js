7fdb847c66b1d7db94069217e05390e530be13c2
Govnor Payne <govnorpayne@gmail.com>
2022-08-17 11:03:55 -0500

    making file reader

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const {
   popOut,
 } = require("./lib/arraySorting");
 
+const calculator = require("./lib/calculator");
+
 const names = ["Alice", "Bob", "Jeremy", "Sam", "Henry", "Sarah", "Ashely"];
 
 console.log("Reverse names: ", Reversed(names));
 console.log("Printed Names: ", Printname(names));
 console.log("Odd Names: ", oddManOut(names));
 console.log("Last name", popOut(names));
+console.log("result", calculator("8 divide 5"));
