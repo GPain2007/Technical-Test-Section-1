ad1568d5f6c45c5f6a08706d22996fcf31fcf0bb
Govnor Payne <govnorpayne@gmail.com>
2022-08-17 11:52:37 -0500

    finished targetAddUp

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const {
 
 const calculator = require("./lib/calculator");
 
+const targetAddUp = require("./lib/targetAddUp");
+
 const names = ["Alice", "Bob", "Jeremy", "Sam", "Henry", "Sarah", "Ashely"];
 
 console.log("Reverse names: ", Reversed(names));
 console.log("Printed Names: ", Printname(names));
 console.log("Odd Names: ", oddManOut(names));
-console.log("Last name", popOut(names));
-console.log("result", calculator("8 divide 5"));
+console.log("Last name: ", popOut(names));
+console.log("Calculator: ", calculator("8 divide 5"));
+console.log("Target Add Up: ", targetAddUp([15, 7, 11, 2], 9));
